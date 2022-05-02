5515e12fb114a1cf1193ed4580996d144cad667d
JoGardhagen <92914305+JoGardhagen@users.noreply.github.com>
2022-05-02 14:26:52 +0200

    initi

---
+++ WpfStopWatch/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfStopWatch
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Time time = new Time ("0:0:0:0"); 
+        private const string clocka = "00:00:00:00";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            clock.Content = clocka.ToString();
+
+            Timer timer = new System.Timers.Timer();
+            timer.Interval = 100;
+            timer.Enabled = true;
+            timer.Elapsed += Timer_Elapsed;
+            timer.Start();
+        }
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            //DateTime myDate = DateTime.Now;
+           Application.Current.Dispatcher.Invoke(() => {
+                time.oneSecondPassed();
+                //if (running == true) { time.oneSecondPassed(); }
+
+                clock.Content = time.getCurrentTime();
+            });
+        }
+        private void startStopClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void resetClick(object sender ,RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WpfStopWatch/Time/Time.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfStopWatch
+{
+    public class Time
+    {
+        private int hour;
+        private int minute;
+        private int second;
+        private int miliSecound;
+
+
+        public Time(int hour, int minute, int second, int miliSecound)
+        {
+            this.hour = hour;
+            this.minute = minute;
+            this.second = second;
+            this.miliSecound = miliSecound;
+        }
+        public Time(string currentTime)
+        {
+            string[] time = currentTime.Split(":");
+            hour = int.Parse(time[0]);
+            minute = int.Parse(time[1]);
+            second = int.Parse(time[2]);
+            miliSecound = int.Parse(time[3]);
+
+        }
+        public string getCurrentTime()
+        {
+            return hour + ":" + minute + ":" + second + ":" + miliSecound;
+        }
+        public string timeReset(string currentTime)
+        {
+            string[] time = currentTime.Split(":");
+            hour = int.Parse(time[0]);
+            minute = int.Parse(time[1]);
+            second = int.Parse(time[2]);
+            miliSecound = int.Parse(time[3]);
+
+
+            //hour = Integer.parseInt(time[0]);
+            //minute = Integer.parseInt(time[1]);
+            //second = Integer.parseInt(time[2]);
+            hour = 0;
+            minute = 0;
+            second = 0;
+            miliSecound = 0;
+            return hour + ":" + minute + ":" + second + ":" + miliSecound;
+        }
+        public void oneSecondPassed()
+        {
+            miliSecound++;
+            if (miliSecound == 10)
+            {
+                miliSecound = 0;
+                second++;
+                if (second == 60)
+                {
+                    minute++;
+                    second = 0;
+                    if (minute == 60)
+                    {
+                        hour++;
+                        minute = 0;
+                        if (hour == 24)
+                        {
+                            hour = 0;
+                            Console.WriteLine("Next day");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
